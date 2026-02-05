3045e86ef50f157a022f70fae2953e5469cecfc2
Dishant Nalwaya <dishantd442442@gmail.com>
2026-02-05 16:35:33 +0530

    fix: desktop file

---
+++ czar_launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Launcher script for CZAR Bootloader
+# This runs the app using the virtual environment
+
+cd /home/czar/app/python_bootloader
+source venv/bin/activate
+python main.py
